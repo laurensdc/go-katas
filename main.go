079b655b610459040a4ef9119bfb98acc5464053
Laurens <laurens.decock@inthepocket.com>
2025-02-20 13:13:07 +0100

    ✨ Add game over state
    - also introduce exciting starting state

--- main.go
+++ main.go
@@ -2,33 +2,53 @@ package main
 
 import (
 	"fmt"
-	"github.com/laurensdc/go-game-of-life/life"
+	"reflect"
 	"time"
+
+	"github.com/laurensdc/go-game-of-life/life"
 )
 
 func main() {
 	ticker := time.NewTicker(100 * time.Millisecond)
 	defer ticker.Stop()
 
-	startingState := [][]bool{
+	state := [][]bool{
 		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
+		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
 		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
 		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
 		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
 		{false, true, true, false, false, true, false, false, false, false, true, true, false, false, true, false, false, false},
 		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, true, false, false, true, false, false, false, false, true, true, false, false, true, false, false, false},
 		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
+		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
+		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
+		{false, true, true, false, false, true, false, false, false, false, true, true, false, false, true, false, false, false},
 		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
+		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
 		{false, true, true, false, false, true, false, true, true, false, true, true, false, false, true, false, true, false},
 	}
 
 	for range ticker.C {
-		fmt.Print("\033[H\033[2J") // clear screen
+		newState := life.Tick(state)
+
+		if reflect.DeepEqual(state, newState) {
+			break // game over because game is not evolving anymome
+		}
+
+		printClearScreen()
+
+		state = newState
+
 		printGameState(state)
 
-		startingState = life.Tick(startingState)
+	}
+}
+
+func printClearScreen() {
+	fmt.Print("\033[H\033[2J") // clear screen
+
+}
 
 func printGameState(state [][]bool) {
 	for i := range state {
@@ -41,4 +61,5 @@ func printGameState(state [][]bool) {
 		}
 		fmt.Println()
 	}
+
 }
