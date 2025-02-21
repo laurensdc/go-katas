90918d2c544fe28dcb2445943b360c11e5ea8121
Laurens <laurens.decock@inthepocket.com>
2025-02-21 16:19:59 +0100

    ♻️ Extract starting state

--- main.go
+++ main.go
@@ -12,23 +12,6 @@ func main() {
 	ticker := time.NewTicker(100 * time.Millisecond)
 	defer ticker.Stop()
 
-	state := [][]bool{
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, true, false, false, true, false, false, false, false, true, true, false, false, true, false, false, false},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, true, false, false, true, false, false, false, false, true, true, false, false, true, false, false, false},
-		{false, false, true, false, false, true, false, true, false, false, true, true, false, false, true, false, true, false},
-		{false, true, false, true, false, true, false, true, false, true, true, false, false, false, false, false, false, true},
-		{false, true, true, false, false, true, false, true, true, false, true, true, false, false, true, false, true, false},
-	}
-
 	for range ticker.C {
 		newState := life.Tick(state)
 
