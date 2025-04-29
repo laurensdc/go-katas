16f8082bcfed866e1015e1fa7ed923a2232b833f
Laurens DC <coding@laurensdc.com>
2025-04-29 22:31:17 +0200

    🐛 Use loop instead of recursion
    - Fixes stackoverflows

--- main.go
+++ main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
+	"bytes"
 	"strconv"
 )
 
 func main() {
+	Counterstring(uint(201))
 
 }
 
 func Counterstring(n uint) string {
-	if n == 0 {
-		return ""
+	var numbers []uint
+
+	i := n
+	for {
+		if i <= 1 {
+			break
+		}
+
+		numbers = append(numbers, i)
+
+		iStr := strconv.FormatUint(uint64(i), 10)
+		numberOfDigits := uint(len(iStr))
+
+		// This calculates the next value for the "recursive" step
+		// Ensure we don't underflow if currentN is small
+		i = i - numberOfDigits - 1
 	}
 
-	if n == 1 {
-		return "*"
+	var buf bytes.Buffer
+
+	if i == 1 {
+		buf.WriteString("*")
 	}
 
-	// e.g. "9999"
-	nStr := strconv.FormatUint(uint64(n), 10)
+	// Append the numbers from numsToAppend in reverse order
+	for i := len(numbers) - 1; i >= 0; i-- {
+		num := numbers[i]
+		buf.WriteString(strconv.FormatUint(uint64(num), 10))
+		buf.WriteString("*")
+	}
 
-	// e.g. "4"
-	numberOfDigits := len(nStr)
-
-	// e.g. Counterstring(9999 - 4 - 1)
-	recursiveCounterstring := Counterstring(n - uint(numberOfDigits) - 1)
-
-	return recursiveCounterstring + nStr + "*"
-
+	return buf.String()
 }
