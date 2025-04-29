949f235aa92dc73e290e745ee9866455bfe502ae
Laurens DC <coding@laurensdc.com>
2025-04-29 10:22:24 +0200

    ✨ Support n >= 100

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func Counterstring(n int) string {
 	if n == 1 {
 		return "*"
 	}
+	if n >= 100 {
+		return Counterstring(n-4) + strconv.Itoa(n) + "*"
+	}
 	if n >= 10 {
 		return Counterstring(n-3) + strconv.Itoa(n) + "*"
 	}
