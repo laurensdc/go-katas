f4e14a1894290e07d74457e4bf83a87777fc1456
Laurens DC <coding@laurensdc.com>
2025-04-29 09:47:43 +0200

    🚧 Add cases for n is 0, 1, 2

--- main.go
+++ main.go
@@ -5,5 +5,12 @@ func main() {
 }
 
 func Counterstring(n int) string {
+	if n == 1 {
+		return "*"
+	}
+
+	if n == 2 {
+		return "2*"
+	}
 	return ""
 }
--- main_test.go
+++ main_test.go
@@ -4,13 +4,32 @@ import (
 	"testing"
 )
 
-func TestOne(t *testing.T) {
+func Test_Zero(t *testing.T) {
 	n := 0
 	got := Counterstring(n)
 	expected := ""
+
 	if got != expected {
 		t.Errorf("Failed for %v\ngot %v\nexpected %v\n", n, got, expected)
 	}
-
 }
 
+func Test_One(t *testing.T) {
+	n := 1
+	got := Counterstring(n)
+	expected := "*"
+
+	if got != expected {
+		t.Errorf("Failed for %v\ngot %v\nexpected %v\n", n, got, expected)
+	}
+}
+
+func Test_Two(t *testing.T) {
+	n := 2
+	got := Counterstring(n)
+	expected := "2*"
+
+	if got != expected {
+		t.Errorf("Failed for %v\ngot %v\nexpected %v\n", n, got, expected)
+	}
+}
